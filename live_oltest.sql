683ad2617bc60a7e284cd13a0af7cf07a23409f0
DESKTOP-S641FLM\yoanc <yoancrayon@gmail.com>
2020-08-19 01:31:11 +0700

    new sql

--- live_oltest.sql
+++ live_oltest.sql
@@ -1,68 +1,33 @@
-/*
-SQLyog Ultimate v12.4.3 (64 bit)
-MySQL - 10.4.13-MariaDB : Database - live_oltest
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 18, 2020 at 08:30 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`live_oltest` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `live_oltest`
+--
+CREATE DATABASE IF NOT EXISTS `live_oltest` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `live_oltest`;
 
-/*Table structure for table `m_jenis_user` */
-
-DROP TABLE IF EXISTS `m_jenis_user`;
-
-CREATE TABLE `m_jenis_user` (
-  `k_jenis_user` varchar(3) NOT NULL,
-  `content` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`k_jenis_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `m_jenis_user` */
-
-insert  into `m_jenis_user`(`k_jenis_user`,`content`) values 
-('1','Admin'),
-('2','Dosen'),
-('3','Mahasiswa');
-
-/*Table structure for table `pengguna` */
-
-DROP TABLE IF EXISTS `pengguna`;
-
-CREATE TABLE `pengguna` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `k_jenis_user` varchar(3) DEFAULT NULL,
-  `nama` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-/*Data for the table `pengguna` */
-
-insert  into `pengguna`(`username`,`password`,`k_jenis_user`,`nama`) values 
-('yogi.kur','12345','1','Yogi Kurniwan');
-
-/* Procedure structure for procedure `delpengguna` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `delpengguna` */;
-
 DELIMITER $$
-
-/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `delpengguna`(
-IN inusername varchar(60)
-
-
-
-)
-begin
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delpengguna` (IN `inusername` VARCHAR(60))  begin
 DECLARE errstate CHAR(5) DEFAULT '00000';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
                        	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
                        	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
@@ -89,17 +54,9 @@ IF errstate='00000' THEN
 	SELECT errstate;
 
 
-end */$$
-DELIMITER ;
+end$$
 
-/* Procedure structure for procedure `getloginstat` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `getloginstat` */;
-
-DELIMITER $$
-
-/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `getloginstat`(in inusername varchar(50), IN inpassword varchar(60))
-BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getloginstat` (IN `inusername` VARCHAR(50), IN `inpassword` VARCHAR(60))  BEGIN
 
 
 	if exists (select * from pengguna where  `username`=inusername and `password`=inpassword) then
@@ -112,41 +69,27 @@ BEGIN
 
 
 
-	END */$$
-DELIMITER ;
+	END$$
 
-/* Procedure structure for procedure `getuserinfo` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `getuserinfo` */;
-
-DELIMITER $$
-
-/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `getuserinfo`(in inusername varchar(60))
-begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getuserinfo` (IN `inusername` VARCHAR(60))  begin
 select a.`username`,a.`nama`,a.`k_jenis_user`,b.`content` as jenis_user from pengguna a, m_jenis_user b
 where a.k_jenis_user=b.k_jenis_user
 and a.`username`=inusername
 ;
 
-end */$$
-DELIMITER ;
+end$$
 
-/* Procedure structure for procedure `insupdpengguna` */
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getuserinfoall` (IN `inusername` VARCHAR(60), `ink_jenis_user` VARCHAR(3))  begin
+select a.`username`,a.`nama`,a.`k_jenis_user`,b.`content` as jenis_user from pengguna a, m_jenis_user b
+where a.k_jenis_user=b.k_jenis_user
+and (a.`username`=inusername or inusername='' or inusername='x' or inusername is null)
+AND (a.`k_jenis_user`=ink_jenis_user OR ink_jenis_user='' OR ink_jenis_user='x' OR ink_jenis_user IS NULL)
 
-/*!50003 DROP PROCEDURE IF EXISTS  `insupdpengguna` */;
+;
 
-DELIMITER $$
+end$$
 
-/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `insupdpengguna`(
-IN inusername varchar(60),
-iN npassword VARCHAR(60),
-IN ink_jenis_user VARCHAR(3),
-IN innama VARCHAR(100)
-
-
-
-)
-begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insupdpengguna` (IN `inusername` VARCHAR(60), IN `inpassword` VARCHAR(60), IN `ink_jenis_user` VARCHAR(3), IN `innama` VARCHAR(100))  begin
 DECLARE errstate CHAR(5) DEFAULT '00000';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
                        	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
                        	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
@@ -165,7 +108,7 @@ INSERT INTO pengguna(`username`,`password`,`k_jenis_user`,`nama`)
 VALUES(inusername,inpassword,ink_jenis_user,innama);
 else
 
-update pengguna set `password`=inpassword,`k_jenis_user`=ink_jenis_user,`nama`=innama
+update pengguna set `k_jenis_user`=ink_jenis_user,`nama`=innama
 where `username`=inusername;
 end if;
 
@@ -177,10 +120,67 @@ IF errstate='00000' THEN
 	SELECT errstate;
 
 
-end */$$
+end$$
+
 DELIMITER ;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `m_jenis_user`
+--
+
+CREATE TABLE `m_jenis_user` (
+  `k_jenis_user` varchar(3) NOT NULL,
+  `content` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `m_jenis_user`
+--
+
+INSERT INTO `m_jenis_user` (`k_jenis_user`, `content`) VALUES
+('1', 'Admin'),
+('2', 'Dosen'),
+('3', 'Mahasiswa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `k_jenis_user` varchar(3) DEFAULT NULL,
+  `nama` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`username`, `password`, `k_jenis_user`, `nama`) VALUES
+('yogi.kur', '12345', '1', 'Yogi Kurniwan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `m_jenis_user`
+--
+ALTER TABLE `m_jenis_user`
+  ADD PRIMARY KEY (`k_jenis_user`);
+
+--
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
