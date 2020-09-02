d7d6b1544b479e90e790b7113922e64fe3f8e4a9
DESKTOP-H5ENQE4\Mamluatul Hani'ah <mamluatul.haniah@gmail.com>
2020-09-02 11:40:42 +0700

    ganti logo

--- app/Views/dashboard_view.php
+++ app/Views/dashboard_view.php
@@ -146,7 +146,7 @@
 	 
       
       </div>
-      <img src="<?php echo base_url('Images/live logo.png'); ?>" class="ml-auto justify-content-center align-items-center mx-auto text-center" />
+      <img src="<?php echo base_url('Images/live logo2.png'); ?>" class="ml-auto justify-content-center align-items-center mx-auto text-center" />
 	   <p class="text-justify font-weight-normal text-monospace">
 	  livE (Online java exercise ) sebagai Sarana meningkatkan kualitas praktikum pada mata kuliah Praktikum Dasar Pemrograman. Tersedianya livE akan memudahkan mahasiswa untuk melakukan praktikum Dasar pemrograman.
 <ol><li> Mahasiswa dapat melakukan langkah-langkah percobaan dan latihan pada website tanpa harus melakukan instalasi perangkat lunak pada masing-masing Komputer mahasiswa</li>
